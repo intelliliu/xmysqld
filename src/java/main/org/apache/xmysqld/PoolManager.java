d6777ea7e86561030ba9c26bd90ce940baa76eb8
intelliliu <invincibleliu00@gmail.com>
2013-12-02 01:01:51 +0800

    made a close circle

--- src/java/main/org/apache/xmysqld/PoolManager.java
+++ src/java/main/org/apache/xmysqld/PoolManager.java
@@ -1,5 +1,9 @@
 package org.apache.xmysqld;
 
+import org.apache.zookeeper.server.quorum.QuorumPeerMain;
+
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * Created with IntelliJ IDEA.
  * User: ye
@@ -8,5 +12,22 @@ package org.apache.xmysqld;
  * To change this template use File | Settings | File Templates.
  */
 public class PoolManager {
+	static {
+		String[] args={"/etc/zoo.cfg"};
+		QuorumPeerMain.main(args);
+	}
 
+	private static ConcurrentHashMap<String,AutoIncrementIDPool> poolManager;
+
+	public static void register(String path, AutoIncrementIDPool pool){
+		poolManager.put(path, pool);
+	}
+
+	public static AutoIncrementIDPool lookup(String path){
+		return poolManager.get(path);
+	}
+
+	public static void main(String[] args){
+		long pk=PoolManager.lookup("/mysql_id/hnair/tb_order/id").getId();
+	}
 }
