bfd1553e7a04be8a28d7a027e30951646c283223
intelliliu <invincibleliu00@gmail.com>
2013-12-02 01:01:05 +0800

    made a close circle

---
+++ src/java/main/org/apache/xmysqld/PoolManager.java
@@ -0,0 +1,12 @@
+package org.apache.xmysqld;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ye
+ * Date: 13-12-1
+ * Time: 下午2:52
+ * To change this template use File | Settings | File Templates.
+ */
+public class PoolManager {
+
+}
